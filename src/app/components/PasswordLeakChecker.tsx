6f3609250b0ec13f828fe7cd011612b26fc64d2c
Jody Frankowski <jody.frankowski@gmail.com>
2025-02-06 16:10:28 +0100

    feat: Add a local top 100 check for PasswordLeakChecker

--- src/app/components/PasswordLeakChecker.tsx
+++ src/app/components/PasswordLeakChecker.tsx
@@ -7,6 +7,111 @@ import { Alert } from '@mui/material';
 
 type matchStatus = '' | '❌' | '😬' | '👍'
 
+// Top 100 passwords in RockYou according to the more realistic SecLists by OWASP
+// https://github.com/danielmiessler/SecLists/blob/fc9cbdfe8f8e5ae0505cd781f6a243239ddddd3f/Passwords/Common-Credentials/10-million-password-list-top-10000.txt#L1
+const rockYouTop100 = [
+  '123456',
+  'password',
+  '12345678',
+  'qwerty',
+  '123456789',
+  '12345',
+  '1234',
+  '111111',
+  '1234567',
+  'dragon',
+  '123123',
+  'baseball',
+  'abc123',
+  'football',
+  'monkey',
+  'letmein',
+  '696969',
+  'shadow',
+  'master',
+  '666666',
+  'qwertyuiop',
+  '123321',
+  'mustang',
+  '1234567890',
+  'michael',
+  '654321',
+  'pussy',
+  'superman',
+  '1qaz2wsx',
+  '7777777',
+  'fuckyou',
+  '121212',
+  '000000',
+  'qazwsx',
+  '123qwe',
+  'killer',
+  'trustno1',
+  'jordan',
+  'jennifer',
+  'zxcvbnm',
+  'asdfgh',
+  'hunter',
+  'buster',
+  'soccer',
+  'harley',
+  'batman',
+  'andrew',
+  'tigger',
+  'sunshine',
+  'iloveyou',
+  'fuckme',
+  '2000',
+  'charlie',
+  'robert',
+  'thomas',
+  'hockey',
+  'ranger',
+  'daniel',
+  'starwars',
+  'klaster',
+  '112233',
+  'george',
+  'asshole',
+  'computer',
+  'michelle',
+  'jessica',
+  'pepper',
+  '1111',
+  'zxcvbn',
+  '555555',
+  '11111111',
+  '131313',
+  'freedom',
+  '777777',
+  'pass',
+  'fuck',
+  'maggie',
+  '159753',
+  'aaaaaa',
+  'ginger',
+  'princess',
+  'joshua',
+  'cheese',
+  'amanda',
+  'summer',
+  'love',
+  'ashley',
+  '6969',
+  'nicole',
+  'chelsea',
+  'biteme',
+  'matthew',
+  'access',
+  'yankees',
+  '987654321',
+  'dallas',
+  'austin',
+  'thunder',
+  'taylor',
+  'matrix',
+]
+
 export default function PasswordLeakChecker() {
   const [password, setPassword] = useState('');
   const [matchStatus, setMatchStatus] = useState<matchStatus>('');
@@ -17,6 +122,11 @@ export default function PasswordLeakChecker() {
 
     if (newPassword === '') {
       setMatchStatus('');
+      return;
+    }
+
+    if (rockYouTop100.includes(newPassword)) {
+      setMatchStatus('😬');
       return;
     }
 
