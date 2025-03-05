8c01501568b17b7e477c618438f0e0122d5747df
Jody Frankowski <jody.frankowski@gmail.com>
2025-03-05 20:00:57 +0100

    fix(front): Replace bad company logo with a proper one

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -14,6 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: 'Have I Been Rocked?',
+  icons: 'data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20viewBox=%220%200%20100%20100%22%3E%3Ctext%20y=%22.9em%22%20font-size=%2290%22%3E🪨%3C/text%3E%3C/svg%3E',
 };
 
 export default function RootLayout({
