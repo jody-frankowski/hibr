5754119ed0ee619c6b676d7b724d6f0c805079e5
Jody Frankowski <jody.frankowski@gmail.com>
2025-03-08 16:26:51 +0100

    fix(front): Make Home() responsive

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -5,7 +5,7 @@ import PasswordGenerator from '@/app/components/PasswordGenerator';
 
 export default function Home() {
   return (
-    <div className="flex items-start">
+    <div className="flex flex-wrap items-start">
       <PasswordLeakChecker />
       <PasswordGenerator />
     </div>
