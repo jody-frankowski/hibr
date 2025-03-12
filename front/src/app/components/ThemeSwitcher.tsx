47ebf5eef5bc7eef86d2b7bd272c7bcadee78c2a
Jody Frankowski <jody.frankowski@gmail.com>
2025-03-12 16:49:42 +0100

    feat(front): Support dark mode system preference and a button to switch

---
+++ front/src/app/components/ThemeSwitcher.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { useTheme } from 'next-themes';
+import { Button } from '@heroui/react';
+import { MoonFilledIcon } from '@heroui/shared-icons';
+
+export function ThemeSwitcher() {
+  const { resolvedTheme, setTheme } = useTheme();
+
+  return (
+    <Button isIconOnly onPress={() => setTheme(resolvedTheme === 'light' ? 'dark' : 'light')}>
+      <MoonFilledIcon />
+    </Button>
+  );
+}
--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -10,10 +10,6 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
     },
   },
   plugins: [heroui()],
